2405eca2c362540510eedf5312f588a85899b133
Onda-dev <manuelaltieri3@gmail.com>
2022-05-04 16:58:58 +0200

    Snack 7 manca lista

---
+++ snack-7/main.js
@@ -0,0 +1,16 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+// Creare una lista con di nomi
+const nameList = ["Manuel", "Stefano", "Pasquale", "Pippo", "Franco"]
+// Creare una lista con di cognomi
+const lastNameList = ["Altieri", "Bianchi", "Perisic", "Brozovic", "Handanovic"]
+// Creare lista con combinazione di nomi e cognomi
+const combinedList = []
+const randomName = nameList[Math.floor(Math.random()*nameList.length)]
+const randomLastname = lastNameList[Math.floor(Math.random()*lastNameList.length)]
+const combined = randomName + randomLastname
+// for (let i = 0; 1 < 10; i++) {
+//     combinedList.push(combined)
+// }
+
+console.log(combined)
