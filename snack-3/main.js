0b5baa0c1221a8f34236f059d348297c9a6eb017
Onda-dev <manuelaltieri3@gmail.com>
2022-05-04 12:16:24 +0200

    Funzionante ma probabilmente sbagliato

---
+++ snack-3/main.js
@@ -0,0 +1,9 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+const firstNumber = Number(prompt("Inserisci il primo numero"))
+const secondNumber = Number(prompt("Inserisci il secondo numero"))
+const thirdNumber = Number(prompt("Inserisci il terzo numero"))
+
+const number = Number(prompt("Inserisci un numero"))
+
+console.log(firstNumber + secondNumber + thirdNumber)
